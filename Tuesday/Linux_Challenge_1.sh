a717a525b6cce7789b0c136dfa29ec683b6a61b6
MTUCyberAcademy <84971521+MTUCyberAcademy@users.noreply.github.com>
2021-06-01 16:13:04 +0100

    Delete Linux_Challenge_1.sh

--- Tuesday/Linux_Challenge_1.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/bash
-FILE=~/Desktop/CyberAcademy/Example.txt
-CODE=Hello
-test -f "$FILE" && echo "Congratulations, Challenge Code 1: CyberSkillsJune2021: CODE" || echo "Sometimes not quite right”

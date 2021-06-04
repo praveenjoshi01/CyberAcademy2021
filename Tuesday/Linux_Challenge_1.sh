0e8114ff84c0f621c9fe448ca0b84d76e012fb3e
MTUCyberAcademy <84971521+MTUCyberAcademy@users.noreply.github.com>
2021-06-04 19:42:43 +0100

    Update Linux_Challenge_1.sh

--- Tuesday/Linux_Challenge_1.sh
+++ Tuesday/Linux_Challenge_1.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-FILE=~/Desktop/Challenges/Example.txt
+FILE=~/Desktop/Challenges/CyberAcademyFile.txt
 CODE=Hello
 if test -f "$FILE"; then
 	echo "Congratulations, Challenge Code 1: CyberSkilsJune2021:$CODE"
